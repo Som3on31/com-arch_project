f81bbee6a12291d647fa6abd14ce4c3a4e0beeaf
Som3on31 <76034350+Som3on31@users.noreply.github.com>
2022-09-24 15:40:06 +0700

    Added some code for debugging

--- Test.java
+++ Test.java
@@ -1,7 +1,23 @@
+import components.Assembler;
+
 public class Test {
 
     public static void main(String[] args) {
+        // String exampleInst = "add 1 0 5";
+        String exampleInst1 = "kek lw 5 e lol";
+        Assembler asb = new Assembler();
 
+        String[] result;
+        try {
+            result = asb.separate(exampleInst1);
+            for (String word : result) {
+                if (word == null)
+                    continue;
+                System.out.println(word);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
