2be7a2c099b21c2d763511a484aeedcdf5f7b91c
Panthonf <panthon_kansap@cmu.ac.th>
2022-09-27 16:00:50 +0700

    change lw inst to binary

--- components/Assembler.java
+++ components/Assembler.java
@@ -46,12 +46,27 @@ public class Assembler {
 
     public void convert(String[] result) throws Exception {
         if (result[1].equals("lw")) { // case lw I-type
-            int r = Integer.parseInt(String.valueOf(result[2]));
-            String rd = Integer.toBinaryString(r);
+            int r1 = Integer.parseInt(String.valueOf(result[2]));
+            int r2 = Integer.parseInt(String.valueOf(result[3]));
+            int r3 = 5;
+            String rd = Integer.toBinaryString(r1);
+            String rs1 = Integer.toBinaryString(r2);
+            String imm = Integer.toBinaryString(r3);
             do {
                 rd = "0" + rd;
             } while (rd.length() < 3); // Check rd bit
+            do {
+                rs1 = "0" + rs1;
+            } while (rs1.length() < 5); // Check rs1 bit
+            do {
+                imm = "0" + imm;
+            } while (imm.length() < 12); // Check imm bit
+            System.out.println("");
+            System.out.println("Ins : " + result[1]);
             System.out.println("rd : " + rd);
+            System.out.println("rs1 : " + rs1);
+            System.out.println("imm : " + imm);
+            System.out.println(imm + rs1 + "010" + rd + "0000011");
         } else {
             System.out.println("error");
         }
