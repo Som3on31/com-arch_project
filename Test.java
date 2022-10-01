46f20c6f98a00cd79c4a0c522f57467514f89f35
Som3on31 <76034350+Som3on31@users.noreply.github.com>
2022-10-01 23:18:30 +0700

    Tested label checker

--- Test.java
+++ Test.java
@@ -1,40 +1,64 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.File;
 
 import components.Assembler;
 
 public class Test {
 
     public static void main(String[] args) throws Exception {
+        LinkedHashMap<String, Integer> savedLabels = new LinkedHashMap<>();
 
-        try {
-            List<String> allLines = Files.readAllLines(Paths.get("./testcases/testcase1.txt"));
-            for (String line : allLines) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // String exampleInst = "add 1 0 5";
         String exampleInst1 = "lw 1 2 3";// lw rt rs imm
-        // String exampleInst = "add 1 2 1";
-        // String exampleInst1 = "lw 0 1 five";
+
         Assembler asb = new Assembler();
         System.out.println();
 
-        String[] result;
+        // String[] result;
         try {
-            result = asb.separate(exampleInst1);
-            for (String word : result) {
-                if (word == null)
-                    continue;
-                System.out.println(word);
-            }
-            asb.convert(result); // convert word to binary
+            int instCount = 0;
+            LinkedList<String> instList = new LinkedList<>();
+            Scanner fileScanner = new Scanner(new File("./testcases/testcase1.txt"));
 
+            String current = fileScanner.nextLine();
+            while (true) {
+                System.out.println(current);
+                instList.add(current);
+                String[] instInParts = asb.separate(current);
+
+                if (instInParts[0] != null) {
+                    if (savedLabels.containsKey(instInParts[0]))
+                        throw new Exception("Duplicate label");
+
+                    if (instInParts[0].length() <= 6)
+                        savedLabels.put(instInParts[0], instCount);
+                    else {
+                        throw new Exception("Labels must not be longer than 6 characters.");
+                    }
+                }
+
+                if (!fileScanner.hasNext())
+                    break;
+                current = fileScanner.nextLine();
+                instCount++;
+            }
+            fileScanner.close();
+
+            for (Map.Entry<String, Integer> e : savedLabels.entrySet()) {
+                System.out.println("Label: " + e.getKey() + " val: " + e.getValue());
+            }
+
+            // result = asb.separate(exampleInst1);
+
+            // for (String word : result) {
+            // if (word == null)
+            // continue;
+            // System.out.println(word);
+            // }
+
+            // asb.convert(result); // convert word to binary
         } catch (Exception e) {
             e.printStackTrace();
         }
