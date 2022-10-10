a54e852b8ccdd7d26253d3f84014057cc30a3dc2
Som3on31 <76034350+Som3on31@users.noreply.github.com>
2022-10-10 15:12:15 +0700

    Add some draft code to run

--- components/Simulator.java
+++ components/Simulator.java
@@ -8,38 +8,32 @@ import java.util.Scanner;
 
 public class Simulator {
     // memory
-    // String[] instCode;
-    long[] instCodeBin;
-
-    long[] registers;
+    int[] instCodeBin;
+    int[] registers;
     HashMap<String, Integer> savedLabels; // save all label's positions
 
-    private String[][] instInParts;
-
-    // private void instCode(){
-
-    // }
     public Simulator() {
         // instCode = new String[Short.MAX_VALUE];
-        instCodeBin = new long[Short.MAX_VALUE];
-        registers = new long[8];
+        instCodeBin = new int[Short.MAX_VALUE];
+        registers = new int[8];
     }
 
-    public void run(String filedesc) {
+    public void run(String filedesc) throws Exception {
         Arrays.fill(registers, 0); // reset all available val in the regs to 0
         Arrays.fill(instCodeBin, 0);
-        savedLabels = new HashMap<>();
+        savedLabels = new HashMap<>(); // clear all saved labels
 
         Assembler asb = new Assembler();
         String[] instCode = new String[Short.MAX_VALUE];
 
+        int instCount = 0;
+
         try {
             Scanner s = new Scanner(new File(filedesc));
             String current = s.nextLine();
-            int instCount = 0;
+            instCode[instCount] = current;
 
             while (current != null) {
-                instCode[instCount] = current;
                 if (!s.hasNext())
                     break;
                 current = s.nextLine();
@@ -55,37 +49,109 @@ public class Simulator {
 
         // TODO separate all inst codes into parts then convert them into binary and hex
         // address
+        String[] instBinStr = asb.massConvert(instCode, savedLabels, instCount);
+        System.arraycopy(instBinStr, 0, instCodeBin, 0, instBinStr.length);
 
         // TODO execute inst given, remember to print error if it does anything
         // undefined in the project spec
 
+        printStateInitial(instBinStr.length);
+        boolean doHalt = false;
+        int instExecutedCount = 0;
+        for (int pc = 0; pc < instCount; pc++) {
+
+            if (doHalt) {
+                System.out.println("machine halted");
+                System.out.println("total of " + instExecutedCount + "instructions executed");
+                System.out.println("final state of machine:");
+            }
+            // if pc is 0 or over the max value of 8-bit integer
+            if (pc < 0 || pc > Short.MAX_VALUE) {
+                if (pc < 0)
+                    throw new Exception("pc underflow");
+                throw new Exception("pc overflow");
+            }
+            printStateRun(pc, instBinStr.length);
+
+            if (doHalt)
+                break;
+
+            // do something
+            String instStr = Assembler.toXBits(Integer.toString(instCodeBin[pc]), 32);
+            String type = instStr.substring(7, 10);
+
+            int rS = binaryToDecimalUnsigned(instStr.substring(10, 14));
+            int rD = binaryToDecimalUnsigned(instStr.substring(13, 16));
+            int imm = binaryToDecimalUnsigned(instStr.substring(16)); // if its value is lower than 7, it will be used
+                                                                      // as rD for R-type instructions
+            switch (type) {
+                case "000" -> {
+                    // add
+                    registers[imm] = registers[rS] + registers[rD];
+                }
+                case "001" -> {
+                    // nand
+                    registers[imm] = ~(registers[rS] & registers[rD]);
+                }
+                case "010" -> {
+                    // lw
+                    registers[rD] = instCodeBin[registers[rS] + imm];
+                }
+                case "011" -> {
+                    // sw
+                    instCodeBin[registers[rS] + imm] = registers[rD];
+                }
+                case "100" -> {
+                    // beq
+                    if (registers[rS] == registers[rD])
+                        pc += 1 + imm;
+                }
+                case "101" -> {
+                    // jalr
+                    registers[rD] = pc + 1;
+                    pc = registers[rS];
+                }
+                case "110" -> {
+                    // halt
+                    doHalt = true;
+                }
+                case "111" -> {
+                    // noop
+
+                }
+
+            }
+
+            instExecutedCount++;
+        }
+
     }
 
     // --------------Used in run to show the current state
-    private void printStateInitial() {
+    private void printStateInitial(int maxSize) {
         // show mem
-
+        for (int i = 0; i < maxSize; i++) {
+            System.out.println("Memory" + "[" + i + "]" + instCodeBin[i]);
+        }
         //
     }
 
-    public void printStateRun(int pc) throws FileNotFoundException {
-
-        File file = new File("./machine_code/mc1.txt");
-        Scanner scan = new Scanner(file);
+    // print @@@ then show the current state of the simulator
+    // example:
+    // http://myweb.cmu.ac.th/sansanee.a/ComputerArchitecture/Project/ExSimulator.txt
+    private void printStateRun(int pc, int maxSize) throws FileNotFoundException {
 
         System.out.println("@@@");
         System.out.println("state:");
         System.out.println("pc" + pc);
         System.out.println("memory:");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("mem" + "[" + i + "]" + scan.nextLine());
+        for (int i = 0; i < maxSize; i++) {
+            System.out.println("mem" + "[" + i + "]" + instCodeBin[i]);
 
         }
         System.out.println("@@@");
-        // print @@@ then show the current state of the simulator
-        // example:
-        // http://myweb.cmu.ac.th/sansanee.a/ComputerArchitecture/Project/ExSimulator.txt
+
     }
 
     // TODO add any private code if necessary, do not forget to comment how it works
@@ -105,7 +171,7 @@ public class Simulator {
      */
     private String[][] saveAllLabelsAndSeparateCode(String[] instCode, Assembler asb) throws Exception {
         int instCount = 0;
-        instInParts = new String[instCode.length][5];
+        String[][] instInParts = new String[instCode.length][5];
         for (int i = 0; i < instCode.length; i++) {
             instInParts[i] = asb.separate(instCode[i]);
 
@@ -126,5 +192,28 @@ public class Simulator {
         // for (Map.Entry<String, Integer> e : savedLabels.entrySet()) {
         // System.out.println("Label: " + e.getKey() + " val: " + e.getValue());
         // }
+    }
+
+    /**
+     * Coverts any binary string into unsigned 31-bit integer.
+     * It will not work if the string is longer than 32 bits. Otherwise, it returns
+     * 0.
+     * 
+     * By Saharit Kadkasem
+     * 
+     * @param bits A binary string
+     * @return A number in the form of unsigned 31-bit integer
+     */
+    private static int binaryToDecimalUnsigned(String bits) {
+        int result = 0;
+        if (bits.length() > 32)
+            return 0;
+
+        for (int i = 0; i < bits.length(); i++) {
+            if (bits.charAt(i) == '1')
+                result += Math.pow(2, bits.length() - i - 1);
+        }
+
+        return result;
     }
 }
